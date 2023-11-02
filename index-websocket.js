98571bb0550868344334624b7c9ef443caac6843
Mary <maria.nicoleta.ion13@gmail.com>
2023-11-02 23:26:16 +0100

    prettier

--- index-websocket.js
+++ index-websocket.js
@@ -1,12 +1,10 @@
-const express = require("express");
-const server = require("http").createServer();
+const express = require('express');
+const server = require('http').createServer();
 const app = express();
 
-app.get("/", function (req, res) {
-  res.sendFile("index.html", { root: __dirname });
+app.get("/",function(req,res){
+    res.sendFile('index.html',{root:__dirname});
 });
 
-server.on("request", app);
-server.listen(3000, function () {
-  console.log("listening on 3000");
-});
+server.on('request',app);
+server.listen(3000,function(){console.log('listening on 3000');});
