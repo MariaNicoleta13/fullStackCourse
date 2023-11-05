95f1bb4063ea27b7891196e49453a68e9b174b63
Mary <maria.nicoleta.ion13@gmail.com>
2023-11-05 11:33:13 +0100

    add websocket

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const http = require("http");
 
-http.createServer( function(req,res){
-res.write("first reply");
-res.end();
-}
-).listen(3000);
-console.log("server started on 3000"); 
+http
+  .createServer(function (req, res) {
+    res.write("first reply");
+    res.end();
+  })
+  .listen(3000);
+console.log("server started on 3000");
--- index-websocket.js
+++ index-websocket.js
@@ -1,10 +1,36 @@
-const express = require('express');
-const server = require('http').createServer();
+const express = require("express");
+const server = require("http").createServer();
 const app = express();
 
-app.get("/",function(req,res){
-    res.sendFile('index.html',{root:__dirname});
+app.get("/", function (req, res) {
+  res.sendFile("index.html", { root: __dirname });
 });
 
-server.on('request',app);
-server.listen(3000,function(){console.log('listening on 3000');});
+server.on("request", app);
+server.listen(3000, function () {
+  console.log("listening on 3000");
+});
+
+/** Begin websocket */
+const WebSocketServer = require("ws").Server;
+const wss = new WebSocketServer({ server: server });
+
+wss.on("connection", function connection(ws) {
+  const numClients = wss.clients.size;
+  console.log("Clients connected ", numClients);
+  wss.broadcast(`current visitors ${numClients}`);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send("welcome to my server");
+  }
+
+  ws.on("close", function close() {
+    console.log("A client has disconnected");
+  });
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
